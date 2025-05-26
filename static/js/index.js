d006e607dc9aae76ec0d393924169192b5bab3ec
zaozao <709800389@qq.com>
2025-05-26 21:12:27 +0800

    commit2

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
 // var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 10;
+var NUM_INTERP_FRAMES = 11;
 
 var interp_images = [];
 function preloadInterpolationImages() {
